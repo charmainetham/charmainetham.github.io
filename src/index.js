e09da95507c90674901a0dc54784a23b1f8c5962
CHARMAINE THAM <charmainetham@gmail.com>
2017-08-02 21:53:29 -0700

    just adding the boiler plate that i've created

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = "Charmaine's portfolio";
+
+ReactDOM.render(
+	<div><h1>{title}</h1></div>,
+	document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+const config ={
+  context: __dirname + '/src',
+  entry: {
+    app: './index.js',
+  },
+  output: {
+    path: __dirname+'/dist',
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules:[
+          	{
+          	  test: /\.js$/,
+          	  exclude: /node_modules/,
+          	  use: 'babel-loader'
+          	}	
+	         ]
+	}	
+};
+
+module.exports  = config;
