7d6c0e71af889bebc57c7461d0d6d151f456ef93
CHARMAINE THAM <charmainetham@gmail.com>
2017-10-19 14:59:47 -0700

    test

--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ const config ={
                 {
                   loader: 'file-loader',
                   options: {
-                    name: '[path][name].[ext]'
+                    name: 'assets/images/[name].[ext]'
                   }  
                 } 
               ]
