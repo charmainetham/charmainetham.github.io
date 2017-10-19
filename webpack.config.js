76418415f0c05c4b0c4e556be9125db86f1741d2
CHARMAINE THAM <charmainetham@gmail.com>
2017-10-19 14:39:48 -0700

    i ran npm build

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,7 @@ const config ={
                 {
                   loader: 'file-loader',
                   options: {
+                    name: '[path][name].[ext]'
                   }  
                 } 
               ]
